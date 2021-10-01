218660e47ec3dcc45b2fe03b87de5f4bca8d3721
Krystian Matkowski <krystianmatkowski@MacBook-Air-Krystian.local>
2021-10-01 22:47:37 +0200

    change funtion displayResult

--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,43 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+
+function getMoveName(arg) {
+	if(arg == '1') {
+		return 'kamień'
+	} else if(arg == '2') {
+		return 'papier'
+	} else if(arg == '3') {
+		return 'nozyce'
+	} else {
+		return 'nieznany ruch'
+	}
+}
+
+function displayResult(argComputerMove, argPlayerMove) {
+
+	if(argPlayerMove == 'kamień' && argComputerMove == 'nozyce' || argPlayerMove == 'papier' && argComputerMove == 'kamień' || argPlayerMove == 'nozyce' && argComputerMove == 'papier' ){
+		printMessage('Ty wygrywasz!');
+	} else if(argPlayerMove == argComputerMove){
+		printMessage('Remis');
+	} else {
+		printMessage('Ty przegrywasz!');
+	}
+}
+
+function playGame(playerInput) {
+    clearMessages()
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+
+    let computerMove = getMoveName(randomNumber);
+    printMessage('Mój ruch to: ' + computerMove);
+
+    
+    let playerMove = getMoveName(playerInput);
+    printMessage('Twój ruch to: ' + playerMove);
+
+
+    displayResult(computerMove, playerMove);
+
+}
