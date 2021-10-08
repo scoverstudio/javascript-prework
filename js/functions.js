583d44a25685b97e4920bfa3a92b729f624c8c92
Krystian Matkowski <krystianmatkowski@MacBook-Air-Krystian.local>
2021-10-08 19:38:13 +0200

    add const to functions

--- js/functions.js
+++ js/functions.js
@@ -1,81 +1,82 @@
 {
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
+	const playerGame = document.querySelector('.result p:nth-child(1) span')
+	const computerGame = document.querySelector('.result p:nth-child(2) span')
+	const drawGames = document.querySelector('.result p:nth-child(3) span')
+	const resultGame = document.querySelector('.result p:nth-child(4) span')
 
-function getMoveName(arg) {
-	if(arg == '1') {
-		return 'kamień'
-	} else if(arg == '2') {
-		return 'papier'
-	} else if(arg == '3') {
-		return 'nozyce'
-	} else {
-		return 'nieznany ruch'
+	function printMessage(msg){
+		let div = document.createElement('div');
+		div.innerHTML = msg;
+		document.getElementById('messages').appendChild(div);
 	}
-}
 
-let computerWins = 0;
-let playerWins = 0;
-let gamesDraws = 0;
-let gamesPlayed = 0;
-
-function displayResult(argComputerMove, argPlayerMove) {
-
-	if(argPlayerMove == 'kamień' && argComputerMove == 'nozyce' || argPlayerMove == 'papier' && argComputerMove == 'kamień' || argPlayerMove == 'nozyce' && argComputerMove == 'papier' ){
-		printMessage('Ty wygrywasz!');
-		playerWins++
-		gamesPlayed++
-	} else if(argPlayerMove == argComputerMove){
-		printMessage('Remis');
-		gamesPlayed++
-		gamesDraws++
-	} else {
-		printMessage('Ty przegrywasz!');
-		computerWins++
-		gamesPlayed++
+	function clearMessages(){
+		document.getElementById('messages').innerHTML = '';
 	}
-}
+
+	const getMoveName = function(arg) {
+		if(arg == '1') {
+			return 'kamień'
+		} else if(arg == '2') {
+			return 'papier'
+		} else if(arg == '3') {
+			return 'nozyce'
+		} else {
+			return 'nieznany ruch'
+		}
+	}
+
+	let computerWins = 0;
+	let playerWins = 0;
+	let gamesDraws = 0;
+	let gamesPlayed = 0;
+
+	const displayResult = function(argComputerMove, argPlayerMove) {
+		if(argPlayerMove == 'kamień' && argComputerMove == 'nozyce' || argPlayerMove == 'papier' && argComputerMove == 'kamień' || argPlayerMove == 'nozyce' && argComputerMove == 'papier' ){
+			printMessage('Ty wygrywasz!');
+			playerWins++
+			gamesPlayed++
+		} else if(argPlayerMove == argComputerMove){
+			printMessage('Remis');
+			gamesPlayed++
+			gamesDraws++
+		} else {
+			printMessage('Ty przegrywasz!');
+			computerWins++
+			gamesPlayed++
+		}
+	}
 
 
-function playGame(playerInput) {
-    clearMessages()
-    let randomNumber = Math.floor(Math.random() * 3 + 1);
+	const playGame = function(playerInput) {
+		clearMessages()
+    	let randomNumber = Math.floor(Math.random() * 3 + 1);
+		let computerMove = getMoveName(randomNumber);
+    	let playerMove = getMoveName(playerInput);
+    	
+		printMessage('Twój ruch to: ' + playerMove);
+		printMessage('Ruch komputera: ' + computerMove);
 
+		displayResult(computerMove, playerMove);	
 
-    let computerMove = getMoveName(randomNumber);
-    printMessage('Ruch komputera: ' + computerMove);
+		playerGame.textContent = playerWins;
+		computerGame.textContent = computerWins;
+		drawGames.textContent = gamesDraws;
+		resultGame.textContent = gamesPlayed;
 
-    
-    let playerMove = getMoveName(playerInput);
-    printMessage('Twój ruch to: ' + playerMove);
+	}
 
+	function restartGame() {
+		computerWins = 0;
+		playerWins = 0;
+		gamesDraws = 0;
+		gamesPlayed = 0;
 
-    displayResult(computerMove, playerMove);
-
-
-	playerGame.textContent = playerWins;
-	computerGame.textContent = computerWins;
-	drawGames.textContent = gamesDraws;
-	resultGame.textContent = gamesPlayed;
-	
-}
-function restartGame() {
-	computerWins = 0;
-	playerWins = 0;
-	gamesDraws = 0;
-	gamesPlayed = 0;
-
-	playerGame.textContent = playerWins;
-	computerGame.textContent = computerWins;
-	drawGames.textContent = gamesDraws;
-	resultGame.textContent = gamesPlayed;
-}
+		playerGame.textContent = playerWins;
+		computerGame.textContent = computerWins;
+		drawGames.textContent = gamesDraws;
+		resultGame.textContent = gamesPlayed;
+	}
 
 }
--- js/script.js
+++ js/script.js
@@ -1,22 +1,20 @@
-const btnRock = document.getElementById('rock');
-const btnPaper = document.getElementById('paper');
-const btnScissors = document.getElementById('scissors');
-const btnRestart = document.querySelector('.restart');
 
-const playerGame = document.querySelector('.result p:nth-child(1) span')
-const computerGame = document.querySelector('.result p:nth-child(2) span')
-const drawGames = document.querySelector('.result p:nth-child(3) span')
-const resultGame = document.querySelector('.result p:nth-child(4) span')
+{
+    const btnRock = document.getElementById('rock');
+    const btnPaper = document.getElementById('paper');
+    const btnScissors = document.getElementById('scissors');
+    const btnRestart = document.querySelector('.restart');
 
-btnRock.addEventListener('click', function(){
-    playGame(1)
-})
-btnPaper.addEventListener('click', function(){
-    playGame(2)
-})
-btnScissors.addEventListener('click', function(){
-    playGame(3)
-})
-btnRestart.addEventListener('click', function(){
-    restartGame()
-})
+    btnRock.addEventListener('click', function(){
+        playGame(1)
+    })
+    btnPaper.addEventListener('click', function(){
+        playGame(2)
+    })
+    btnScissors.addEventListener('click', function(){
+        playGame(3)
+    })
+    btnRestart.addEventListener('click', function(){
+        restartGame()
+    })
+}
